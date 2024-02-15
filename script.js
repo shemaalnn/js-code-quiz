619522b620cfd09bf7bf8b344dd80e187beb1f81
Shema <shema.aln@outlook.com>
2024-02-15 23:37:28 +0000

    Completed with basic css

--- script.js
+++ script.js
@@ -2,108 +2,123 @@ var questions = [
     {
         question: 'Commonly used data types DO NOT include:',
         options: ['strings', 'booleans', 'alerts', 'numbers'],
-        answer: 'alerts',
+        answer: 'alerts'
     },
     {
         question: 'The condition in an if / else statement is enclosed within ______',
         options: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
-        answer: 'parentheses',
+        answer: 'parentheses'
     },
     {
         question: 'Arrays in JavaScript can be used to store _____.',
         options: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
-        answer: 'all of the above',
+        answer: 'all of the above'
     },
     {
         question: 'String values must be enclosed within ___ when being assigned to variables',
         options: ['commas', 'curly brackets', 'quotes', 'parentheses'],
-        answer: 'quotes',
+        answer: 'quotes'
     },
     {
         question: 'How do you declare a variable in JavaScript?',
         options: ['var', 'variable', 'v', 'let'],
-        answer: 'var',
+        answer: 'var'
     },
     {
         question: 'Which keyword is used to define a function in JavaScript?',
         options: ['function', 'def', 'func', 'define'],
-        answer: 'function',
+        answer: 'function'
     }
-]; 
+];
 
-var beginQuizEl = document.getElementById("begin");
+// Variables
+var beginQuizEl = document.getElementById("beginQuiz");
 var welcomeEl = document.getElementById("welcomescreen");
 var quizEl = document.getElementById("questionscreen");
-var scoreEl = document.getElementById("scores");
 var optionsEl = document.getElementById("options");
 var resultEl = document.getElementById("result");
 var questionsEl = document.getElementById("questions");
 var countdownEl = document.getElementById("countdown");
 var answerEl = document.getElementById("correct");
+var timerEl = document.getElementById("timer");
 var messageEl = document.getElementById("message");
-var button = createOptionButton(options);
-var button = document.createElement('button');
 
 var score = 0;
-var timeLeft = 100;
+var secondsLeft = 0; 
 var currentQuestion = 0;
+var countdownTimer;
 
-var countdownTimer; 
+function stopQuiz() {
+    clearInterval(countdownTimer);
 
-function beginQuiz() {
-    welcomeEl.style.display = "none";
-    quizEl.style.display = "block";
-    presentQuestion();
-    startTimer();
+    timerEl.textContent = "";
+if (resultEl) {
+    resultEl.style.display = 'block';
+}
+if (quizEl) {
+    quizEl.style.display = 'none';
+}
+    messageEl.textContent = "You scored " + score;
 }
 
-function presentQuestion() {
-    var currentQuestionDisplayed = questions[currentQuestion];
-    questionsEl.textContent = currentQuestionDisplayed.question;
+function nextQuestion() {
+    presentQuestion();
+}
 
-    optionsEl.textContent = '';
-    currentQuestionDisplayed.options.forEach(function(options) {
+// Starting the quiz
+function startQuiz() {
+    secondsLeft = 100;
+    score = 0;
+    currentQuestion = -1;
+
+    countdownTimer = setInterval(function () {
+        if (secondsLeft > 0) {
+            timerEl.textContent = secondsLeft;
+            secondsLeft--;
+        } else {
+            stopQuiz();
+        }
+    }, 1000);
+
+    presentQuestion();
+}
+
+// Presenting a question
+function presentQuestion() {
+    currentQuestion++;
+
+    if (currentQuestion >= questions.length) {
+        stopQuiz();
+        return;
+    }
+
+    var question = questions[currentQuestion];
+    questionsEl.textContent = question.question;
+    optionsEl.innerHTML = '';
+
+    question.options.forEach(function (option) {
+        var button = createOptionButton(option);
         optionsEl.appendChild(button);
     });
 }
 
-function createOptionButton(options) {
-    button.textContent = options;
-    button.onclick = function() {
-    checkAnswer(options);
-}; 
-return button;
+function createOptionButton(option) { 
+    var button = document.createElement('button');
+    button.textContent = option;
+    button.onclick = function () {
+        checkAnswer(option);
+    };
+    return button;
 }
 
-function checkAnswer(selectedOption) { 
-    var currentQuestionDisplayed = questions[currentQuestion];
-    if (selectedOption == currentQuestionDisplayed.answer) {
-        score++; 
+function checkAnswer(selectedOption) {
+    var question = questions[currentQuestion];
+
+    if (selectedOption === question.answer) {
+        score++;
     }
-    resultEl.textContent = "Your score = " + score;
-    currentQuestion++; 
-    if (currentQuestion < questions.length) {
-        presentQuestion();
-    } else {
-        endQuiz();
-    }
+
+    presentQuestion();
 }
 
-function startTimer() {
-    countdownTimer = setInterval(function() { 
-        timeLeft--;
-        countdownEl.textContent = "Time Remaining = " + timeLeft + " seconds";
-        if (timeLeft <= 0) {
-            endQuiz();
-        }
-    }, 1000);
-}
-
-
-function endQuiz() {
-    quizEl.style.display = "none";
-    scoreEl.style.display = "block";
-    messageEl.textContent = "Well done! Your total score is = " + score;
-}
-
-beginQuizEl.addEventListener("click", beginQuiz);
+beginQuizEl.addEventListener("click", startQuiz);
